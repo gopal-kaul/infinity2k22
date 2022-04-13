187439ec9bc535aa43dbeb6a9f853b287551b8c7
Gopal Kaul <gopalkaul5@gmail.com>
2022-04-13 16:55:11 +0530

    Added 404 route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import HomePage from "./components/Homepage";
@@ -9,6 +9,7 @@ import ContactUs from "./components/ContactUs";
 import Events from "./components/Events";
 import ScrollToTop from "./components/ScrollToTop";
 import { Container } from "react-bootstrap";
+import FourOhFour from "./components/404";
 function App() {
   return (
     <BrowserRouter>
@@ -21,6 +22,7 @@ function App() {
             <Route path="/workshop" element={<Workshop />} />
             <Route path="/contact" element={<ContactUs />} />
             <Route path="/events" element={<Events />} />
+            <Route path="*" element={<FourOhFour />} />
           </Routes>
         </Container>
       </div>
