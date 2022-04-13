933339290a6ed0e178935e65618e405468fefc90
Gopal Kaul <gopalkaul5@gmail.com>
2022-04-14 00:13:41 +0530

    Add Sponsors and Gallery Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { HashRouter, Route, Routes} from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import HomePage from "./components/Homepage";
@@ -10,6 +10,8 @@ import Events from "./components/Events";
 import ScrollToTop from "./components/ScrollToTop";
 import { Container } from "react-bootstrap";
 import FourOhFour from "./components/404";
+import Sponsors from "./components/Sponsors";
+import Gallery from "./components/Gallery";
 function App() {
   return (
     <HashRouter>
@@ -22,6 +24,8 @@ function App() {
             <Route path="/workshop" element={<Workshop />} />
             <Route path="/contact" element={<ContactUs />} />
             <Route path="/events" element={<Events />} />
+            <Route path="/sponsors" element={<Sponsors />} />
+            <Route path="/gallery" element={<Gallery />} />
             <Route path="*" element={<FourOhFour />} />
           </Routes>
         </Container>
