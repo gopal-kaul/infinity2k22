744e0642add91402d764a9fb80767a32157ccab6
Gopal Kaul <gopalkaul5@gmail.com>
2022-04-15 16:52:04 +0530

    Build Gallery Component and add appropriate styles

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,5 +1,15 @@
+import { Carousel, Image } from "react-bootstrap";
 export default function Gallery() {
   return (
-    <div>Gallery</div>
-  )
+    <div>
+      <h2>Infinity 2k20</h2>
+      <Carousel>
+        {[...Array(9)].map((i, idx) => (
+          <Carousel.Item key={idx}>
+            <Image className="d-block w-100 galleryimg" src={`gallery/${idx + 1}.webp`} />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
+  );
 }
