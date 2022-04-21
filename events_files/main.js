194a0df918e54075a3b92766b0c9ca519d50caae
Gopal Kaul <gopalkaul5@gmail.com>
2022-04-21 14:31:44 +0530

    Added static page for QR compatibility

---
+++ events_files/main.js
@@ -0,0 +1,19 @@
+var _jsxFileName = "/home/bl4z3/infinity2k22/src/main.jsx";
+import __vite__cjsImport0_react from "/node_modules/.vite/deps/react.js?v=8920a220"; const React = __vite__cjsImport0_react.__esModule ? __vite__cjsImport0_react.default : __vite__cjsImport0_react;
+import __vite__cjsImport1_reactDom from "/node_modules/.vite/deps/react-dom.js?v=8920a220"; const ReactDOM = __vite__cjsImport1_reactDom.__esModule ? __vite__cjsImport1_reactDom.default : __vite__cjsImport1_reactDom;
+import "/src/index.css";
+import App from "/src/App.jsx?t=1650531492796";
+import __vite__cjsImport4_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=8920a220"; const _jsxDEV = __vite__cjsImport4_react_jsxDevRuntime["jsxDEV"];
+ReactDOM.render(/* @__PURE__ */ _jsxDEV(React.StrictMode, {
+  children: /* @__PURE__ */ _jsxDEV(App, {}, void 0, false, {
+    fileName: _jsxFileName,
+    lineNumber: 8,
+    columnNumber: 5
+  }, this)
+}, void 0, false, {
+  fileName: _jsxFileName,
+  lineNumber: 7,
+  columnNumber: 3
+}, this), document.getElementById("root"));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IjtBQUFBO0FBQ0E7QUFDQTtBQUNBOztBQUVBQSxTQUFTQyxPQUNQLHdCQUFDLE1BQU0sWUFBUDtBQUFBLFlBQ0Usd0JBQUMsS0FBRDtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBREY7QUFBQTtBQUFBO0FBQUE7QUFBQSxVQUdBQyxTQUFTQyxlQUFlLE1BQXhCLENBSkYiLCJuYW1lcyI6WyJSZWFjdERPTSIsInJlbmRlciIsImRvY3VtZW50IiwiZ2V0RWxlbWVudEJ5SWQiXSwic291cmNlcyI6WyIvaG9tZS9ibDR6My9pbmZpbml0eTJrMjIvc3JjL21haW4uanN4Il0sImZpbGUiOiIvaG9tZS9ibDR6My9pbmZpbml0eTJrMjIvc3JjL21haW4uanN4Iiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFJlYWN0IGZyb20gJ3JlYWN0J1xuaW1wb3J0IFJlYWN0RE9NIGZyb20gJ3JlYWN0LWRvbSdcbmltcG9ydCAnLi9pbmRleC5jc3MnXG5pbXBvcnQgQXBwIGZyb20gJy4vQXBwJ1xuXG5SZWFjdERPTS5yZW5kZXIoXG4gIDxSZWFjdC5TdHJpY3RNb2RlPlxuICAgIDxBcHAgLz5cbiAgPC9SZWFjdC5TdHJpY3RNb2RlPixcbiAgZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoJ3Jvb3QnKVxuKVxuIl19
